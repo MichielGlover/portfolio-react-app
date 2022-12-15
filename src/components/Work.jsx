5a57fe0668f6c8bdd4451a45c5abc4f05afc05d7
Michiel Glover <michielglover@hotmail.com>
2022-12-15 22:48:41 +0100

    Project added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import WorkImg from "../assets/projects/workImg.jpeg";
+import powerLijstImg from "../assets/projects/Powerlijst.png";
 import realEstate from "../assets/projects/realestate.jpg";
 
 const Work = () => {
@@ -14,17 +14,40 @@ const Work = () => {
         </div>
 
         {/* Container */}
-        <div className="grid sm:grid-cols-1 md:grid-cols-1 gap-4">
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
 
             {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${powerLijstImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                REACT JS Applicatie
+                POWERLIJST
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://MichielGlover.github.io/powerlist-react-app">
+                  <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/MichielGlover/powerlist-react-app">
+                  <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${realEstate})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+              BMI-Calculator
               </span>
               <div className="pt-8 text-center">
                 <a href="">
@@ -40,29 +63,6 @@ const Work = () => {
               </div>
             </div>
           </div>
-          {/* <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            *Hover Effects*
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                REACT JS Applicatie
-              </span>
-              <div className="pt-8 text-center">
-                <a href="">
-                  <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="">
-                  <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
