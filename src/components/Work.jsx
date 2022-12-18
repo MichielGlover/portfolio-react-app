6365813e8c64a66a13e89466789dbcfc20cd61ad
Michiel Glover <michielglover@hotmail.com>
2022-12-18 20:04:04 +0100

    dark theme project added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import bmiCalculatorImg from "../assets/projects/BMI-Calculator.png";
 import powerLijstImg from "../assets/projects/Powerlijst.png";
+import darkThemeImg from "../assets/projects/DarkThemeSwitch.png";
 
 import realEstate from "../assets/projects/realestate.jpg";
 
 const Work = () => {
   return (
-    <div name="work" className="w-full h-screen md:h-screen text-gray-300 bg-[#0a192f]">
+    <div
+      name="work"
+      className="w-full h-screen md:h-screen text-gray-300 bg-[#0a192f]"
+    >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className=" text-4xl font-bold inline border-b-4 text-gray-300 border-yellow-400">
@@ -18,7 +22,32 @@ const Work = () => {
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
 
-            {/* Grid Item */}
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${darkThemeImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Dark Theme Toggle
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://michielglover.github.io/dark-theme-react-app/">
+                  <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/MichielGlover/dark-theme-react-app">
+                  <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${powerLijstImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -42,6 +71,7 @@ const Work = () => {
               </div>
             </div>
           </div>
+          {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${bmiCalculatorImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -49,7 +79,7 @@ const Work = () => {
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-              BMI-Calculator
+                BMI-Calculator
               </span>
               <div className="pt-8 text-center">
                 <a href="https://michielglover.github.io/bmi-calculator-javascript/">
